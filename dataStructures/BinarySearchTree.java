685e3ec542a066009921f685ba67030d3e309bc3
LMuchapirei <h170407h@hit.ac.zw>
2023-11-24 18:50:34 +0200

    Implement the insert procedure

--- dataStructures/BinarySearchTree.java
+++ dataStructures/BinarySearchTree.java
@@ -1,7 +1,7 @@
 
 
 
-
+// we need things that are comparable so that we can rank them, and pick if they can go right or left
 public class BinarySearchTree<T extends Comparable<T>> {
     // Tracks the number of nodes in the BST
     private int nodeCount = 0;
@@ -20,4 +20,47 @@ public class BinarySearchTree<T extends Comparable<T>> {
             this.right=right;
         }
     }
+
+    // Check if this binary tree is empty
+    public boolean isEmpty(){
+        return size()==0;
+    }
+
+    // Get the number of nodes in this binary tree
+    public int size(){
+        return nodeCount;
+    }
+
+    // Add an element to this binary tree. Return true if we have successfully perform an insertion
+    public boolean add(T elem){
+        // Check if the value already exists in this binary tee , if it does ignore it
+        if(contains(elem)){
+            return false;
+        // otherwise add this element to the binary tree
+        }else{
+            root = add(root,elem);
+            nodeCount++;
+            return true;
+        }
+    }
+
+    // Method to recursively add a value in the binary tree
+    private Node add(Node node, T elem){
+        // Base case: found a leaf node
+        if(node==null){
+            node=new Node(null,null,elem);
+        }else{
+          // place lower elements values in left subtree
+          if(elem.compareTo(node.data) < 0){
+            node.left = add(node.left,elem);
+          }else{
+            node.right = add(node.right,elem);
+          }
+        }
+        return node;
+    }
+
+    
+    
+
 }
